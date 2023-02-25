66fda94366f86d8fbc7279002b6f5d58efb84ce9
Mariana Oliveira <marianaoliveirafernandes@alu.ufc.br>
2023-02-25 15:19:27 -0300

    app da home

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -11,6 +12,71 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.grey[900],
+      appBar: AppBar(
+        brightness: Brightness.dark,
+        backgroundColor: Colors.grey[900],
+        elevation: 0,
+        leading: Icon(Icons.search),
+        actions: [
+          Column(
+            mainAxisSize: MainAxisSize.min,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Olá, Mari",
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Text(
+                "Brasil",
+                style: TextStyle(fontSize: 10),
+              )
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 8, left: 15),
+            child: Icon(
+              Icons.notifications_active_outlined,
+              size: 30,
+            ),
+          )
+        ],
+      ),
+      body: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text("mais",
+                      style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20),
+                  child: Text("popular",
+                      style: TextStyle(
+                          fontSize: 50,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white)),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20, top: 10, bottom: 20),
+                  child: Text("960 playlist",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, color: Colors.grey)),
+                )
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
